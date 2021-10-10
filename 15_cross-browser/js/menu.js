b84d68fcad79eac5b8be6cf9aad2264e76e61b52
Albert Gabbasov <albertxgm@gmail.com>
2021-10-10 03:34:44 +0300

    fix: Евклид

--- 15_cross-browser/js/menu.js
+++ 15_cross-browser/js/menu.js
@@ -2,5 +2,6 @@ const navButton = document.querySelector('.header__mobile-navbutton');
 const navBar = document.querySelector('.header__mobile-navbar');
 
 navButton.addEventListener('click', () => {
+  navButton.classList.toggle('header__mobile-navbutton_show');
   navBar.classList.toggle('header__mobile-navbar_show');
 })
